85aab6e102a8f1e105683cfbd21ac2cf7dfb29da
ankit <ankit.nimje93@gmail.com>
2025-05-25 00:09:58 -0400

    add sample logo

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './logo'
+
+const NavBar = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/navbar/logo.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <div className="flex items-center cursor-pointer" onClick={() => router.push('/')}>
+      <span className="text-2xl font-bold text-primary-900">J&K</span>
+      <span className="text-xl text-primary-700 ml-1">Cabinets</span>
+    </div>
+  );
+};
+
+export default Logo;
