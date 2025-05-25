e0caa0ae2e4401d9766b29ad64d07c385050883a
ankit <ankit.nimje93@gmail.com>
2025-05-24 22:16:20 -0400

    add missing next import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import next from 'eslint-config-next';
 
 
 export default defineConfig([
