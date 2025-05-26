b6c63a86c9506c332aae3c6dfbb06748266a514c
ankit <ankit.nimje93@gmail.com>
2025-05-26 15:31:48 -0400

    fix logo layout

--- src/components/navbar/logo.client.tsx
+++ src/components/navbar/logo.client.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 const Logo = () => {
   const router = useRouter();
   return (
-    <div className="flex items-center cursor-pointer" onClick={() => router.push('/')}>
+    <div className="flex items-center gap-2 cursor-pointer" onClick={() => router.push('/')}>
       <Image
         src="/assets/logo-main.png"
         alt="J&K Cabinetry Logo"
@@ -16,7 +16,7 @@ const Logo = () => {
         className="h-8 w-auto"
         priority
       />
-            <span className="text-2xl font-bold text-brand">J&K Cabinetry</span>
+      <span className="text-2xl font-bold text-brand">J&K Cabinetry</span>
     </div>
   );
 };
