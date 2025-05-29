fda52c70a9af28d36694490c18d81a933e5d2708
ken20022 <84741815+ken20022@users.noreply.github.com>
2025-05-29 11:10:16 -0500

    added some comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,32 @@ export default function Home() {
         <Hero />
       </section>
 
-      {/* Product Styles Section - Placeholder */}
+      {/* Most Loved Styles Section - Placeholder  reference: https://c21b33-75.myshopify.com/*/}
+      {/*White Shaker (S8) 
+         Dove (E1)
+         Charcoal (E2)
+         Sage (E3)
+         Pebble (B6)
+         Pure (B5)*/}
       <section className="py-12">
-        Product styles
+        Most Loved Styles
+      </section>
+
+      {/* Explore More Styles Section - Placeholder reference: https://c21b33-75.myshopify.com/*/}
+      {/*Naval (S7) 
+         Butterscotch (B8)
+         Java Coffee (S1)
+         Castle Grey (S5)
+         Greige (K3)
+         Espresso (K8)
+         Mocha Glazed (K10)
+         Hazel (H8)
+         Pearl Glazed (H9)
+         Creme Glazed (A7)
+         Mahogany (J5)
+         Chocolate Glazed (M01)*/}
+      <section className="py-12">
+        Explore More Styles
       </section>
     </div>
   );
