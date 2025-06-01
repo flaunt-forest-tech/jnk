14f56cd97820c59ff084396329beb140707ed39f
Abhishek Goenka <govindkaran@gmail.com>
2025-06-01 09:56:29 -0500

    Other styles

--- src/components/cabinet-styles/best-selling.tsx
+++ src/components/cabinet-styles/best-selling.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StyleThumbnail from './style-thumbnail';
 import { CABINET_STYLES } from '@/constants/data/door-styles.types';
 
-const BestSellingDoorStyle: React.FC = () => {
+const BestSellingCabinetStyle: React.FC = () => {
   return (
     <div>
       <h2 className="font-bold  text-5xl text-black-900 text-center">Best Selling Styles</h2>
@@ -15,4 +15,4 @@ const BestSellingDoorStyle: React.FC = () => {
   );
 };
 
-export default BestSellingDoorStyle;
+export default BestSellingCabinetStyle;
--- src/constants/data/door-styles.types.ts
+++ src/constants/data/door-styles.types.ts
@@ -14,13 +14,15 @@ export const CABINET_STYLES: CabinetStyleType[] = [
   { name: 'Pebble', code: 'B6', fileName: 'B6-Pebble.png', bestSelling: true, order: 5 },
   { name: 'Pure', code: 'B5', fileName: 'B5-Pure.png', bestSelling: true, order: 6 },
 
-  // { name: 'Naval', code: "S7", fileName: 'S7-Naval.png', order: 7 },
-  // { name: 'Butterscotch', code: "B8", fileName: 'B8-Butterscotch.png', order: 8 },
+  { name: 'Naval', code: 'B7', fileName: 'B7-Naval.png', order: 7 },
+  { name: 'Butterscotch', code: 'B8', fileName: 'B8-Butterscotch.png', order: 8 },
+  { name: 'Greige', code: 'K3', fileName: 'K3-Greige.png', order: 9 },
+  { name: 'Espresso', code: 'K8', fileName: 'K8-Espresso.png', order: 10 },
+  { name: 'Mocha Glazed', code: 'K10', fileName: 'K10-MochaGlazed.png', order: 11 },
+
   // { name: 'Java Coffee', code: "S1", fileName: 'S1-JavaCoffee.png', order: 9 },
   // { name: 'Castle Grey', code: "S5", fileName: 'S5-CastleGrey.png', order: 10 },
-  // { name: 'Greige', code: "K3", fileName: 'K3-Greige.png', order: 11 },
-  // { name: 'Espresso', code: "K8", fileName: 'K8-Espresso.png', order: 12 },
-  // { name: 'Mocha Glazed', code: "K10", fileName: 'K10-MochaGlazed.png', order: 13 },
+
   // { name: 'Hazel', code: "H8", fileName: 'H8-Hazel.png', order: 14 },
   // { name: 'Pearl Glazed', code: "H9", fileName: 'H9-PearlGlazed.png', order: 15 },
   // { name: 'Creme Glazed', code: "A7", fileName: 'A7-CremeGlazed.png', order: 16 },
