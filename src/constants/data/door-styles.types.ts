a35f4408ac32b31a06017708c4ecf88c3dcd6f50
ken20022 <84741815+ken20022@users.noreply.github.com>
2025-06-02 21:07:11 -0500

    Renew Styles color pictures

--- src/constants/data/door-styles.types.ts
+++ src/constants/data/door-styles.types.ts
@@ -7,24 +7,29 @@ export interface CabinetStyleType {
 }
 
 export const CABINET_STYLES: CabinetStyleType[] = [
-  { name: 'White Shaker', code: 'S8', fileName: 'S8-WhiteShaker.png', bestSelling: true, order: 1 },
-  { name: 'Dove', code: 'E1', fileName: 'E1-Dove.png', bestSelling: true, order: 2 },
-  { name: 'Charcoal', code: 'E2', fileName: 'E2-Charcoal.png', bestSelling: true, order: 3 },
-  { name: 'Sage', code: 'E3', fileName: 'E3-Sage.png', bestSelling: true, order: 4 },
-  { name: 'Pebble', code: 'B6', fileName: 'B6-Pebble.png', bestSelling: true, order: 5 },
-  { name: 'Pure', code: 'B5', fileName: 'B5-Pure.png', bestSelling: true, order: 6 },
+  {
+    name: 'White Shaker',
+    code: 'S8',
+    fileName: 'WHITE SHAKER - S8.jpg',
+    bestSelling: true,
+    order: 1,
+  },
+  { name: 'Dove', code: 'E1', fileName: 'DOVE - E1.jpg', bestSelling: true, order: 2 },
+  { name: 'Charcoal', code: 'E2', fileName: 'CHARCOAL - E2.jpg', bestSelling: true, order: 3 },
+  { name: 'Sage', code: 'E3', fileName: 'SAGE - E3.jpg', bestSelling: true, order: 4 },
+  { name: 'Pebble', code: 'B6', fileName: 'PEBBLE - B6.jpg', bestSelling: true, order: 5 },
+  { name: 'Pure', code: 'B5', fileName: 'PURE - B5.jpg', bestSelling: true, order: 6 },
 
-  { name: 'Naval', code: 'B7', fileName: 'B7-Naval.png', order: 7 },
-  { name: 'Butterscotch', code: 'B8', fileName: 'B8-Butterscotch.png', order: 8 },
-  { name: 'Greige', code: 'K3', fileName: 'K3-Greige.png', order: 9 },
-  { name: 'Espresso', code: 'K8', fileName: 'K8-Espresso.png', order: 10 },
-  { name: 'Mocha Glazed', code: 'K10', fileName: 'K10-MochaGlazed.png', order: 11 },
+  { name: 'Naval', code: 'B7', fileName: 'NAVAL - B7.jpg', order: 7 },
+  { name: 'Butterscotch', code: 'B8', fileName: 'BUTTERSCOTCH - B8.jpg', order: 8 },
+  { name: 'Java Coffee', code: 'S1', fileName: 'JAVA COFFEE - S1.jpg', order: 9 },
+  { name: 'Castle Grey', code: 'S5', fileName: 'CASTLE GREY - S5.jpg', order: 10 },
+  { name: 'Greige', code: 'K3', fileName: 'GREIGE - K3.jpg', order: 11 },
+  { name: 'Espresso', code: 'K8', fileName: 'ESPRESSO - K8.jpg', order: 12 },
 
-  // { name: 'Java Coffee', code: "S1", fileName: 'S1-JavaCoffee.png', order: 9 },
-  // { name: 'Castle Grey', code: "S5", fileName: 'S5-CastleGrey.png', order: 10 },
-
-  // { name: 'Hazel', code: "H8", fileName: 'H8-Hazel.png', order: 14 },
-  // { name: 'Pearl Glazed', code: "H9", fileName: 'H9-PearlGlazed.png', order: 15 },
-  // { name: 'Creme Glazed', code: "A7", fileName: 'A7-CremeGlazed.png', order: 16 },
-  // { name: 'Mahogany', code: "J5", fileName: 'J5-Mahogany.png', order: 17 }
+  { name: 'Mocha Glazed', code: 'K10', fileName: 'MOCHA GLAZED - K10.jpg', order: 13 },
+  { name: 'Hazel', code: 'H8', fileName: 'HAZEL - H8.jpg', order: 14 },
+  { name: 'Pearl Glazed', code: 'H9', fileName: 'PEARL GLAZED - H9.jpg', order: 15 },
+  { name: 'Creme Glazed', code: 'A7', fileName: 'CREME GLAZE - A7.jpg', order: 16 },
+  { name: 'Mahogany', code: 'J5', fileName: 'MAHOGANY - J5.jpg', order: 17 },
 ];
