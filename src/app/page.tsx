e2e1943894077f5cac82ab00f1589ab5a6ead354
ankit <ankit.nimje93@gmail.com>
2025-08-30 14:04:30 -0400

    add cabinet styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import CabinetStyles from '@/components/cabinet-styles/cabinet-styles';
+import { loadCabinetStyles } from '@/lib/styles.server';
 
-export default function Home() {
+export default async function Home() {
+  const styles = await loadCabinetStyles();
   return (
     <div className="flex flex-col min-h-screen">
       <section className="py-12">
-        <CabinetStyles styles={[]} />
+        <CabinetStyles styles={styles} />
       </section>
     </div>
   );
